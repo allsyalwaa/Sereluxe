683bf2d7ddfc85903f64c8740150c9a0a5594580
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 16:13:24 +0700

    feat: Add Product page component and route to App component

---
+++ src/pages/Product.jsx
@@ -0,0 +1,10 @@
+import MainLayout from "../layout/MainLayout";
+import SecProduct from "../components/section/SecProduct";
+
+export default function Product() {
+  return (
+    <MainLayout>
+      <SecProduct />
+    </MainLayout>
+  );
+}
