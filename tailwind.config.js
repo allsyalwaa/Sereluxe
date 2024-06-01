88b67b26c1c98976c7191b7c67e8fc76782bca3b
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 14:42:49 +0700

    feat: Update tailwind.config.js with new colors and container styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2DD1C7",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "112px",
+      },
+      center: true,
+    },
   },
   plugins: [],
 };
