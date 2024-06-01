b19fe2e8226f1df6048bffd0869fd672d36e50bd
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 16:33:29 +0700

    feat: Add popup component for product images in SecProduct component

--- src/components/section/SecProduct.jsx
+++ src/components/section/SecProduct.jsx
@@ -1,24 +1,72 @@
 import CardProduct from "../ui/CardProduct";
 import CatalogProduct from "../ui/CatalogProduct";
 
+import { useState } from "react";
+
 export default function SecProduct() {
+  const [isProductPopupOpen, setIsProductPopupOpen] = useState(false);
+
+  const handleOpenProductPopup = () => {
+    setIsProductPopupOpen(true);
+  };
+
+  const handleCloseProductPopup = () => {
+    setIsProductPopupOpen(false);
+  };
   return (
     <section className="container my-12">
       <div className="grid grid-cols-4 gap-8">
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
+
+        <button onClick={handleOpenProductPopup}>
+          <CardProduct />
+        </button>
       </div>
-      <CatalogProduct />
+      {isProductPopupOpen && (
+        <CatalogProduct onClose={handleCloseProductPopup} />
+      )}
     </section>
   );
 }
