8572a8f6ddf3c1566cfe93826361eff4ff3d080f
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 16:13:29 +0700

    feat: Add SecProduct component for displaying multiple product cards

---
+++ src/components/section/SecProduct.jsx
@@ -0,0 +1,22 @@
+import CardProduct from "../ui/CardProduct";
+
+export default function SecProduct() {
+  return (
+    <section className="container my-12">
+      <div className="grid grid-cols-4 gap-8">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
+    </section>
+  );
+}
