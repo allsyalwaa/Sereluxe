faa5ac8711fbb0bf49c468dfe329c2195d563586
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 15:48:04 +0700

    feat: Add Login page component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import SecLogin from "../components/section/SecLogin";
+import MainLayout from "../layout/MainLayout";
+
+export default function Login() {
+  return (
+    <MainLayout>
+      <SecLogin />
+    </MainLayout>
+  );
+}
