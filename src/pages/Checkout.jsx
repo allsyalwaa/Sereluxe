9a5934b3f480a299167a509f9937f5812abd5b61
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 17:01:59 +0700

    feat: Add Checkout page component and route to App component

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,10 @@
+import SecCheckout from "../components/section/SecCheckout";
+import MainLayout from "../layout/MainLayout";
+
+export default function Checkout() {
+  return (
+    <MainLayout>
+      <SecCheckout />
+    </MainLayout>
+  );
+}
