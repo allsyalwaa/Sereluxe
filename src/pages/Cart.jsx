1f5b123d13f669ee9e5ffdae4863a64cd066f6af
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 16:33:10 +0700

    feat: Create Cart page component

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,10 @@
+import SecCart from "../components/section/SecCart";
+import MainLayout from "../layout/MainLayout";
+
+export default function Cart() {
+  return (
+    <MainLayout>
+      <SecCart />
+    </MainLayout>
+  );
+}
