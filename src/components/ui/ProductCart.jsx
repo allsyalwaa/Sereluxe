d659c05aed2f7084fd3fcfa5f24125761711c47f
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 16:32:56 +0700

    feat: Add ProductCart component for displaying product details in the cart

---
+++ src/components/ui/ProductCart.jsx
@@ -0,0 +1,49 @@
+import Product2 from "../../assets/product2.svg";
+import Button from "./Button";
+
+export default function ProductCart() {
+  return (
+    <div className="flex flex-col gap-3 rounded-xl bg-white p-4 shadow">
+      <img src={Product2} alt="" />
+      <h1 className="text-xl font-semibold text-black">Nama product</h1>
+      <div className="flex items-center justify-between">
+        <p className="text-primary text-xl font-medium">$5.00</p>
+        <div className="flex items-center gap-2 rounded-full border-[1.5px] border-black/50 p-1">
+          <div className="rounded-full bg-black/50 p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.8em"
+              height="0.8em"
+              viewBox="0 0 24 24"
+              className="text-white"
+            >
+              <path
+                fill="currentColor"
+                d="M18 12.998H6a1 1 0 0 1 0-2h12a1 1 0 0 1 0 2"
+              />
+            </svg>
+          </div>
+          <p className="text-sm font-medium text-black/50">1</p>
+          <div className="rounded-full bg-black/50 p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.8em"
+              height="0.8em"
+              viewBox="0 0 24 24"
+              className="text-white"
+            >
+              <path
+                fill="currentColor"
+                d="M18 12.998h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5v-5a1 1 0 0 1 2 0v5h5a1 1 0 0 1 0 2"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <p className="flex justify-end text-sm font-medium">$Totalprice</p>
+      <Button className={"w-full"} variant="primary">
+        Buy Now
+      </Button>
+    </div>
+  );
+}
