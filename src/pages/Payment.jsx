48a959fbbd87317028365e2173831e161ec2e1b0
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-01 17:03:22 +0700

    feat: Add Payment page component and route to App component

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,10 @@
+import SecPayment from "../components/section/SecPayment";
+import MainLayout from "../layout/MainLayout";
+
+export default function Payment() {
+  return (
+    <MainLayout>
+      <SecPayment />
+    </MainLayout>
+  );
+}
