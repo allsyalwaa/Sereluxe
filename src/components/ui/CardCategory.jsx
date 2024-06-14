ed64a6c897a9d5e7ffa4cd27ed7ad87adff7842b
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-14 09:26:37 +0700

    refactor: Update CardCategory component to accept image and name props

--- src/components/ui/CardCategory.jsx
+++ src/components/ui/CardCategory.jsx
@@ -1,10 +1,9 @@
-import Product3 from "../../assets/product3.svg";
-
-export default function CardCategory() {
+/* eslint-disable react/prop-types */
+export default function CardCategory({ image, name }) {
   return (
     <div className="mt-4 flex items-center gap-6">
-      <img className="h-16" src={Product3} alt="" />
-      <p className="text-block text-lg font-semibold">Merk product</p>
+      <img className="h-16" src={image} alt="" />
+      <p className="text-block text-lg font-semibold uppercase">{name}</p>
     </div>
   );
 }
