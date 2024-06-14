ddebf916b8affbe45c39df11b0372fe53c08986e
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-14 10:20:49 +0700

    refactor: Update SecCheckout component to use new product image, name, and price

--- src/components/section/SecCheckout.jsx
+++ src/components/section/SecCheckout.jsx
@@ -1,22 +1,24 @@
-import Product3 from "../../assets/product3.svg";
+import checkout from "../../assets/checkout.svg";
 import Bri from "../../assets/bri.svg";
+import bca from "../../assets/bca.svg";
+import mandiri from "../../assets/mandiri.svg";
 import Button from "../ui/Button";
 
 export default function SecCheckout() {
   return (
     <section className="container my-12">
       <div className="grid grid-cols-2 gap-8">
-        <div className="border-primary rounded-xl border-[1.5px] p-6">
-          <div className="grid grid-cols-2 items-center gap-6">
-            <img src={Product3} alt="" />
+        <div>
+          <div className="grid grid-cols-2 items-center gap-6 rounded-xl border-[1.5px] border-primary p-6">
+            <img src={checkout} alt="" />
             <div className="flex flex-col gap-3">
-              <h1 className="text-xl font-bold text-black">Nama product</h1>
+              <h1 className="text-xl font-bold text-black">Cleansing Gel</h1>
               <p className="text-base font-medium">Qty : 3</p>
-              <p className="text-primary text-lg font-semibold">$Total</p>
+              <p className="text-lg font-semibold text-primary">$71.97</p>
             </div>
           </div>
         </div>
-        <div className="border-primary rounded-xl border-[1.5px] p-6">
+        <div className="rounded-xl border-[1.5px] border-primary p-6">
           <h1 className="font-medium italic">Pay with</h1>
           <form action="" className="mt-4 flex flex-col gap-4">
             <div className="flex items-center gap-4">
@@ -25,11 +27,11 @@ export default function SecCheckout() {
             </div>
             <div className="flex items-center gap-4">
               <input type="radio" name="payment" id="payment1" />
-              <img src={Bri} alt="" />
+              <img src={bca} alt="" />
             </div>
             <div className="flex items-center gap-4">
               <input type="radio" name="payment" id="payment1" />
-              <img src={Bri} alt="" />
+              <img src={mandiri} alt="" />
             </div>
           </form>
           <Button type="submit" variant="primary" className="mt-6 w-full">
